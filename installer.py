04eef6036a9b40dbca7060498c1953e81c08228e
Zia456-sys <ziacherry21@gmail.com>
2024-09-24 15:07:42 +0700

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,196 @@
+import os
+
+os.system ("clear")
+print ("""
+
+
+
+        SC MODIFIKASI OLEH "unknown"
+         >>>>> MINTA IJIN DULU YA
+ HARGAI ORANG YANG SUDAH CAPEK BIKIN YA...
+
+
+
+
+""")
+
+print ("""pakai mana masbro?\n \033[32m[1] Termux\033[32m\n \033[32m[2] nethunter\033[32m\n""")
+
+c = input(">>>: ")
+if c == "1":
+    os.system ("pkg install x11-repo -y")
+    os.system ("pkg update && pkg upgrade -y")
+    #os.system ("pkg install python3 -y")
+    os.system ("pkg install -y pypy3-venv")
+    #os.system ("pkg upgrade python3 -y")
+    os.system ("pkg install pip -y")
+    os.system ("pkg install wget -y")
+    os.system ("pkg install gem -y")
+    os.system ("pkg install nmap -y")
+    os.system ("pkg install -y mailutils")
+    os.system ("pkg install -y nodejs")
+    os.system ("pkg install -y openssl")
+    os.system ("pkg install -y perl")
+    os.system ("pkg install -y curl")
+    os.system ("pkg install -y sql-injection")
+    os.system ("pip install requests")
+    os.system ("pip install thread")
+    os.system ("pip install colorama")
+    os.system ("pip install socks")
+    os.system ("pip install cluster")
+    os.system ("pip install crypto")
+    os.system ("pip install pysocks")
+    os.system ("pip install pyqt5")
+    os.system ("pip install httpx")
+    os.system ("pip install cloudscraper")
+    os.system ("pip install cfscrape")
+    os.system ("pip install times")
+    os.system ("pip install bs4")
+    os.system ("pip install mechanize")
+    os.system ("pip install undetected_chromedriver")
+    os.system ("pip install ipaddress")
+    os.system ("pip install webhooks")
+    os.system ("pip install pyuseragents")
+    os.system ("pip install user-agents")
+    os.system ("pip install netlib")
+    os.system ("pip install pyproject.toml")
+    os.system ("pip install dnspython")
+    os.system ("pip install scapy")
+    os.system ("pip install mechanize")
+    os.system ("pip install ipaddress")
+    os.system ("pip install axios")
+    os.system ("pip install path")
+    os.system ("pkg install openjdk-17 -y")
+    os.system ("pkg install golang -y")
+    os.system ("pip install --upgrade pip")
+    os.system ("apt-get --fix-broken install")
+    os.system ("apt-get update && apt-get full-upgrade -y")
+    os.system ("apt-get dist-upgrade -y")
+    os.system ("apt-get autoremove")
+    os.system ("chmod +777 run")
+    os.system ("npm install axios")
+    os.system ("npm install user-agents")
+    os.system ("npm install requests")
+    os.system ("npm install https-proxy-agent")
+    os.system ("npm install crypto-random-string")
+    os.system ("npm install events")
+    os.system ("npm install fs")
+    os.system ("npm install net")
+    os.system ("npm install cloudscraper")
+    os.system ("npm install hcaptcha-solver")
+    os.system ("npm install randomstring")
+    os.system ("npm install cluster")
+    os.system ("npm install cloudflare-bypasser")
+    os.system ("clear")
+    
+    
+elif c == "2":
+    os.system ("sudo apt update && sudo apt full-upgrade -y")
+    #os.system ("sudo apt install -y kali-linux-default")
+    os.system ("sudo rm -rf /var/lib/dpkg/info/postgresql* && sudo dpkg --configure -a")
+    os.system ("sudo apt install wget -y")
+    os.system ("sudo apt install build-essential zlib1g-dev libncurses-devlib libgdbm-dev libnss3 libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev -y")
+    os.system ("wget https://www.python.org/ftp/python/3.14.0/Python-3.14.0.tgz")
+    os.system ("tar -xvf Python-3.14.0.tgz")
+    os.system ("rm Python-3.14.0.tgz")
+    os.system ("cd Python-3.14.0 && sudo ./configure --enable-optimizations && sudo make install")
+    #os.system ("sudo apt upgrade python3 -y")
+    os.system ("sudo dpkg --configure -a")
+    os.system ("sudo apt install python-is-python3 -y")
+    os.system ("sudo apt install -y pypy3-venv")
+    os.system ("sudo apt install -y pipx")
+    os.system ("sudo apt install pip -y")
+    os.system ("sudo apt install gem -y")
+    os.system ("sudo apt install nmap -y")
+    os.system ("sudo apt install mailcap -y")
+    #os.system ("sudo apt install sparrow-wifi -y")
+    os.system ("sudo apt install openssl -y")
+    os.system ("sudo apt install nodejs -y")
+    os.system ("sudo apt install perl -y")
+    os.system ("sudo apt install curl -y")
+    os.system ("sudo apt install -y sql-injection")
+    os.system ("sudo apt install -y figlet")
+    os.system ("sudo apt install -y toilet")
+    os.system ("pip install requests")
+    os.system ("pip install colorama")
+    os.system ("pip install thread")
+    os.system ("pip install netlib")
+    os.system ("pip install pyproject.toml")
+    os.system ("pip install dnspython")
+    os.system ("pip install socks")
+    os.system ("pip install pyroxy")
+    os.system ("pip install pysocks")
+    os.system ("pip install pyqt5")
+    os.system ("pip install pyuseragents")
+    os.system ("pip install user-agents")
+    os.system ("pip install httpx")
+    os.system ("pip install cloudscraper")
+    os.system ("pip install times")
+    os.system ("pip install bs4")
+    os.system ("pip install mechanize")
+    os.system ("pip install undetected_chromedriver")
+    os.system ("pip install ipaddress")
+    os.system ("pip install scapy")
+    os.system ("pip install --upgrade pip")
+    os.system ("pip install webhooks")
+    os.system ("pip install cluster")
+    os.system ("pip install crypto")
+    os.system ("pip install axios")
+    os.system ("pip install path")
+    os.system ("sudo apt --fix-broken install")
+    os.system ("sudo apt update && sudo apt full-upgrade -y")
+    os.system ("sudo apt dist-upgrade -y")
+    os.system ("sudo apt install default-jdk -y")
+    os.system ("sudo apt install golang -y")
+    os.system ("sudo apt install -y npm")
+    os.system ("sudo apt autoremove")
+    os.system ("sudo chmod +777 run")
+    os.system ("npm install axios")
+    os.system ("npm install user-agents")
+    os.system ("npm install requests")
+    os.system ("npm install https-proxy-agent")
+    os.system ("npm install crypto-random-string")
+    os.system ("npm install events")
+    os.system ("npm install fs")
+    os.system ("npm install net")
+    os.system ("npm install cloudscraper")
+    os.system ("npm install hcaptcha-solver")
+    os.system ("npm install randomstring")
+    os.system ("npm install cluster")
+    os.system ("npm install cloudflare-bypasser")
+    os.system ("clear")
+    
+elif c == "":
+    print ("""
+        LHO KOK GA JADI .....??
+        
+        LUPA APA SALAH?
+        
+        YASUDAH KALAU GA JADI, GA APA APA KOK
+        
+        """)
+    exit()
+    
+if os.name == "nt":
+    pass
+    
+else:
+    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+    os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
+
+os.system ("clear")
+    
+print ("""
+
+
+
+
+
+    SUDAH SELESAI KAKA
+    
+    
+    
+    
+    
+    
+    """)
